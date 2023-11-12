73539307dbd3762f0ef3c99c49daec93ab2319e5
PipinZz <gabripip44@gmail.com>
2023-11-12 21:41:57 +0100

    iniziata pagina profilo

--- src/app/pagine/profilo/profilo.component.ts
+++ src/app/pagine/profilo/profilo.component.ts
@@ -1,4 +1,8 @@
 import { Component } from '@angular/core';
+import { SupabaseService } from 'src/app/servizi/supabase.service';
+import { MenuService } from 'src/app/servizi/menu.service';
+import { Router } from '@angular/router';
+
 
 @Component({
   selector: 'app-profilo',
@@ -6,5 +10,34 @@ import { Component } from '@angular/core';
   styleUrls: ['./profilo.component.css']
 })
 export class ProfiloComponent {
+  immagineProfilo: any;
+  profileSession: any;
+  username : any;
+
+  constructor(
+    public servizioMenu: MenuService,
+    private supabaseService: SupabaseService,
+    private router: Router
+    
+  ) {
+    this.getProfilePic();
+    this.setProfileData();
+  }
+
+
+
+  //////query //////
+
+  async getProfilePic() {
+    this.immagineProfilo =  await this.supabaseService.getProfilePic()
+  }
+
+  async setProfileData() {
+    let sessionData =  await this.supabaseService.getSession()
+    this.profileSession =  sessionData.session?.user
+    this.username = sessionData.session?.user.user_metadata['username']
+  }
+
+
 
 }
