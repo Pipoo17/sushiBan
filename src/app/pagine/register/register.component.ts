8a2067883f72deb6f80539b025d9b30136633af2
PipinZz <gabripip44@gmail.com>
2024-01-09 22:54:19 +0100

    mesasggio errore registrazione mail gia esistente

--- src/app/pagine/register/register.component.ts
+++ src/app/pagine/register/register.component.ts
@@ -1,7 +1,10 @@
+
 import { Component } from '@angular/core';
 import { NgForm } from '@angular/forms';
 import { MenuService } from 'src/app/servizi/menu.service';
 import { Router } from '@angular/router';
+import { SupabaseService } from 'src/app/servizi/supabase.service';
+import { MessageService } from 'src/app/servizi/message.service';
 
 @Component({
   selector: 'app-register',
@@ -9,52 +12,82 @@ import { Router } from '@angular/router';
   styleUrls: ['./register.component.css']
 })
 export class RegisterComponent {
-  constructor(private servizioMenu: MenuService,private router: Router){}
+  //constructor(private servizioMenu: MenuService,private router: Router){}
+  
+  constructor(
+    public servizioMenu: MenuService,
+    private supabaseService: SupabaseService,
+    private router: Router,
+    private MessageService: MessageService,
+    
+  ) {}
   message: string = '';
+  authError : boolean = false;
+  isLoading : boolean = false;
+  isBeforeLoading : boolean = true;
 
-  onSubmit(form: NgForm) {
-    const email = form.value.email;
-    const password = form.value.password;    
-    const username = form.value.username;
-    const nome = form.value.nome;
-    const cognome = form.value.cognome;
+onSubmit(form: NgForm){
+  this.isLoading= true;
+  this.isBeforeLoading= false;
+  
+  try{
+    let paramJson = form.value;
+    this.supabaseService.register(paramJson)
+      .then((data) => {
+        this.isLoading = false;
+        if (data.success == false) {
+          this.authError = true,
+          console.error("ERRORE : ", data.description);
+          this.MessageService.showMessageError('',this.getMessageError(data.description))
+        }else {
+          this.isLoading = false;
+          this.MessageService.showMessageInfo('',"Conferma la mail per completare la registrazione")
+          console.log('Conferma Mail')
+      // Puoi gestire la conferma dell'email o altre azioni qui
+    }
 
-    this.servizioMenu.signUp(email, password)
-      .subscribe(
-        () => {
-          this.message ='Registrazione avvenuta con successo!';
-           this.router.navigate(['/login'], { queryParams: { registrationSuccess: true } });
-          form.reset();
-        },
-        error => {
-          //console.error('Errore durante la registrazione:', error);
-          const errorMessage = error?.error?.error?.message
-          if (errorMessage === 'EMAIL_EXISTS') {
-            this.message ="La mail inserita è già in uso.";
-          } 
-          else if (password.length < 6) {
-            this.message ="La Password deve contenere almeno 6 caratteri.";
-          } 
-          else {
-            this.message ='Si è verificato un errore durante la registrazione.';
-          }
-          console.log(this.message)
-        }
-      );
+    this.router.navigate(['/register']);
+
+      })
+      .catch((error) => {
+        this.isLoading = false;
+        console.error("Errore durante l'inserimento:", error);
+      });
+  }catch(error){
+    console.error("Errore durante l'inserimento:", error);
+    this.isLoading = false;
   }
 }
 
 
+getMessageError(descErrore : any){
+  if (descErrore == 'Password should be at least 6 characters'){
+    return "La password deve avere alemno 6 caratteri";
+  }
+  else if (descErrore == 'Unable to validate email address: invalid format'){
+    return"Email non valida.";
+  }
+  else if (descErrore == 'User already registered'){
+    return"Questo utente è gia registrato.";
+  }
+  else if (descErrore == 'duplicate key value violates unique constraint "profiles_username_key'){
+    return "Esiste già un utente con questo Username.";
+  }
+  else if (descErrore == 'Email rate limit exceeded'){
+    return "Troppe richieste in arrivo : riprova tra un po." ;
+  }
+  else if (descErrore == 'insert or update on table "profiles" violates foreign key constraint "profiles_id_fkey"'){
+    return "Esiste già un profilo con quella email" ;
+  }
+  else{
+    return "Errore nella registrazione : "+ descErrore;
+  }
+}
 
-/*
 
-        error => {
-          //console.error('Errore durante la registrazione:', error);
-          if (error?.error?.error?.message === 'EMAIL_EXISTS') {
-            this.message ="La mail inserita è già in uso.";
-          } 
-          else {
-            this.message ='Si è verificato un errore durante la registrazione.';
-          }
-          console.log(this.message)
-        }*/
+}
+
+
+
+
+
